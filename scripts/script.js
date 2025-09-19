ff864d7d99525efc67e3b6d5cf72785b151a8427
lightdarkmaster <christian.barbosa05222001@gmail.com>
2025-09-19 14:34:27 +0800

    Fix some bugs

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const displayChat = () => {
+    window.$zoho = window.$zoho || {};
+    window.$zoho.salesiq = window.$zoho.salesiq || {
+        ready: function(fn) {
+            if (document.readyState !== 'loading') {
+                fn();
+            } else {
+                document.addEventListener('DOMContentLoaded', fn);
+            }
+        }
+    };
+    const script = document.createElement('script');
+    script.src = 'https://salesiq.zohopublic.com/widget?wc=siq21e41ed3314d52339322acd54166cd348f3b37e5c78fc51975d58e98549c98e8';
+    script.defer = true;
+    document.getElementById('zsiqscript').parentNode.appendChild(script);
+}
