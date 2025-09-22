a19478c9f6f2d9741a69f351b2086cbf9c281ffc
lightdarkmaster <christian.barbosa05222001@gmail.com>
2025-09-22 08:16:35 +0800

    Modified scripts for some reason

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,20 @@ const displayChat = () => {
     script.defer = true;
     document.getElementById('zsiqscript').parentNode.appendChild(script);
 }
+
+ const showChat = () => {
+    window.$zoho = window.$zoho || {};
+    window.$zoho.salesiq = window.$zoho.salesiq || {
+        ready: function(fn) {
+            if (document.readyState !== 'loading') {
+                fn();
+            } else {
+                document.addEventListener('DOMContentLoaded', fn);
+            }
+        }
+    };
+    const script = document.createElement('script');
+    script.src = 'https://salesiq.zohopublic.com/widget?wc=siq21e41ed3314d52339322acd54166cd348f3b37e5c78fc51975d58e98549c98e8';
+    script.defer = true;
+    document.getElementById('zsiqscript').parentNode.appendChild(script);
+}
