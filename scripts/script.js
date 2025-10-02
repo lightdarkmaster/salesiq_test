d068c3059f0ad21bccdf66ec30517d876fb91bea
lightdarkmaster <christian.barbosa05222001@gmail.com>
2025-10-02 09:19:28 +0800

    Remove Unuse Lines of Code causing conflict

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,7 @@
-const addNumbers = (firstNumber, secondNumber) => {
-  const sum = firstNumber + secondNumber;
-  return sum;
-}
-
-const createElement = () => {
-  const script = document.createElement("script");
-  script.src =
-    "https://salesiq.zohopublic.com/widget?wc=siq21e41ed3314d52339322acd54166cd348f3b37e5c78fc51975d58e98549c98e8";
-  script.defer = true;
-  document.getElementById("zsiqscript").parentNode.appendChild(script);
-}
-
-const save = () => {
-  createElement();
-}
-
-save();
-
-const recalculate = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
-}
-
-console.clear();
-
-console.log(reacalculate());
-
-const recalibrate = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
-}
-
-if (recalibrate) {
-  recalibrate();
-}
-
-console.log(recalibrate());
-
-const  calculate = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
-}
-
-const getDataperCount =()=>{
-  const age = document.getElementById("num1").value;
-  const count = document.getElementById("num2").value;
-  getData(age, count);
-}
-
-const generate = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
+const getData = (num1, num2) => {
+  const age = num1;
+  const count = num2;
+  // do something with age and count
 }
 
 const generateData = () => {
@@ -63,16 +10,5 @@ const generateData = () => {
   getData(num1, num2);
 }
 
-const getData = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
-}
-
-const get = () => {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  getData(num1, num2);
-}
-
-console.log(get());
+// Call generateData when needed
+generateData();
