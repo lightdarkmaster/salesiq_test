30f1886b344d54c35c702bdadf30774abbf2ca5b
lightdarkmaster <christian.barbosa05222001@gmail.com>
2025-10-03 13:55:26 +0800

    Add some functions

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,19 @@
 document.getElementById("button").addEventListener("click", function() {
   alert("Button clicked!");
+  console.log("Button clicked!");
+  changeBackgroundColor();
 });
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
