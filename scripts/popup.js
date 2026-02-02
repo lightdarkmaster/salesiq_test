ca327ff8370bcc549963dd87a57f278f0b71baae
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-02-02 17:04:25 +0800

    Update Code

--- scripts/popup.js
+++ scripts/popup.js
@@ -19,4 +19,10 @@ function repeatChangeColor(params) {
   changeBackgroundColorComplex(params.times);
 }
 
+function changeBackgroundColor() {
+  var colors = ["#FF5733", "#33FF57", "#3357FF", "#F333FF", "#33FFF5"];
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
+}
+
 //Still not working..
